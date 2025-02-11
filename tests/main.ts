948a44f58ba94de7a7cd2c01ded2f9a05a031573
Cameron Tacklind <cameron@tacklind.com>
2025-02-11 01:10:29 -0800

    cleanup

--- tests/main.ts
+++ tests/main.ts
@@ -1,7 +1,6 @@
 import { wait } from '../src/wait';
-import process from 'process';
-import cp from 'child_process';
-import path from 'path';
+import { execSync, ExecSyncOptions } from 'node:child_process';
+import { join } from 'node:path';
 
 describe('standalone-stats', () => {
   test('throws invalid number', async () => {
@@ -18,15 +17,15 @@ describe('standalone-stats', () => {
   });
 
   if (process.env.GITHUB_ACTIONS) {
-  // shows how the runner will run a javascript action with env / stdout protocol
-  test('test runs', () => {
-    process.env['INPUT_MILLISECONDS'] = '500';
-    const ip = path.join(__dirname, '..', 'lib', 'main.js');
-    const options: cp.ExecSyncOptions = {
-      env: process.env,
-    };
-    // eslint-disable-next-line no-console
-    console.log(cp.execSync(`node ${ip}`, options).toString());
-  });
+    // shows how the runner will run a javascript action with env / stdout protocol
+    test('test runs', () => {
+      process.env['INPUT_MILLISECONDS'] = '500';
+      const ip = join(__dirname, '..', 'dist', 'index.js');
+      const options: ExecSyncOptions = {
+        env: process.env,
+      };
+      // eslint-disable-next-line no-console
+      console.log(execSync(`node ${ip}`, options).toString());
+    });
   }
 });
